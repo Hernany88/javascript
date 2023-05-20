14cf0bf0249c0cea77f752531d4ca0c91a3a6087
Hernany88 <115047694+Hernany88@users.noreply.github.com>
2023-05-20 17:23:53 -0100

    for ex

---
+++ aula14/ambiente.js
@@ -0,0 +1,28 @@
+//for
+console.log('Vai começar...')
+for(var c = 1 ;c <= 5 ;c++){
+     console.log(c)
+}
+console.log('FIM!')
+
+
+/*do while
+var c = 1
+
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+*/
+
+
+/*while
+var c = 1
+
+while (c <= 5){
+    console.log(c)
+    c++
+}
+
+*/
